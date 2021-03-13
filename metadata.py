b14ed0c20f2b6b99626a20bf4debb6bf82b0a384
DasLampe <daslampe@lano-crew.org>
2021-03-13 14:27:07 +0100

    Fixed: Add docker group to users that already has sudo permission

--- metadata.py
+++ metadata.py
@@ -33,3 +33,20 @@ def add_apt_packages(metadata):
             }
         }
     }
+
+@metadata_reactor
+def add_users_to_docker_group(metadata):
+    if not node.has_bundle('users'):
+        raise DoNotRunAgain
+
+    processed_metadata = {
+        'users': {}
+    }
+
+    for username, userdata in metadata.get('users').items():
+        if userdata.get('sudo', False):
+            processed_metadata['users'][username] = {
+                'add_groups': ['docker']
+            }
+
+    return processed_metadata
